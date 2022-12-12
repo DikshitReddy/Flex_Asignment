3b37eb78f4c1066b8e20f095edd3c4e6635b252a
sel1471 <60206728+DikshitReddy@users.noreply.github.com>
2022-12-12 16:11:40 +0530

    Add files via upload

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="yogaHeader">
+            <h>Yoga Classes Enroll Form</h>
+        </div>
+        
+    )
+}
+
+export default Header
